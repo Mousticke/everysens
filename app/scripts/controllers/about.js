c39ea5e9573e4143b40ba1f7855a96032e72f365
Benchiha <akim.benchiha@gmail.com>
2017-05-11 14:17:04 +0200

    Initialisation du projet

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name everysensApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the everysensApp
+ */
+angular.module('everysensApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name everysensApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the everysensApp
+ */
+angular.module('everysensApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
